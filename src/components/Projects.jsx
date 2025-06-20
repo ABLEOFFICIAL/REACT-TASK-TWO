f488588547a281d7d44a79bc135ed109e6e0628b
ABLEOFFICIAL <ekwesijoshua@gmail.com>
2025-06-21 06:00:46 +0800

    wip

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,30 +9,51 @@ import { ArrowRIght } from "./HeroSection";
 
 const Projects = () => {
   return (
-    <section className="flex flex-col justify-between h-[765px] mt-20 max-w-[1170px] mx-auto">
-      <h1 className="text-[64px] font-light leading-[64px] text-[#BDBDBD]">
+    <section className="flex flex-col justify-between h-auto lg:h-[765px] mt-20 max-w-[1170px] mx-auto px-4">
+      <h1 className="text-[32px] sm:text-[40px] md:text-[48px] lg:text-[64px] font-light leading-[1.2] text-[#BDBDBD]">
         Our Projects
       </h1>
-      <div className="parent">
+
+      <div className="parent flex flex-col lg:flex-row flex-wrap gap-4 mt-6">
+        {/* Div 1 - Sample Project */}
         <div
-          style={{ background: `url(${Rectangle12})` }}
-          className="div1 w-[570px] h-[255px] text-[#FFFFFF] relative"
+          style={{
+            background: `url(${Rectangle12})`,
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+          className="div1 w-full lg:w-[570px] h-[200px] sm:h-[255px] text-[#FFFFFF] relative"
         >
-          <div className="bg-black/70 absolute top-0 left-0 h-full w-full flex flex-col justify-center items-center">
-            <h1 className="w-[233px] font-[700] text-[64px] leading-[64px] ">
+          <div className="bg-black/70 absolute top-0 left-0 h-full w-full flex flex-col justify-center items-center px-4 text-center">
+            <h1 className="w-full sm:w-[233px] font-[700] text-[32px] sm:text-[48px] lg:text-[64px] leading-tight mb-4">
               Sample Project
             </h1>
-            <MyBtn classname="py-3 px-5">
+            <MyBtn classname="py-2 px-4 sm:py-3 sm:px-5">
               VIEW MORE <ArrowRIght classname="inline" />
             </MyBtn>
           </div>
         </div>
-        <img src={Rectangle15} className="div2  w-[570px] h-[255px]" />
-        <img src={Rectangle16} className="div3 w-[270px] h-[255px]" />
-        <img src={Rectangle17} className="div4 w-[470px] h-[255px]" />
-        <img src={Rectangle18} className="div5 w-[370px] h-[255px]" />
+
+        {/* Images */}
+        <img
+          src={Rectangle15}
+          className="div2 w-full lg:w-[570px] h-[200px] sm:h-[255px] object-cover"
+        />
+        <img
+          src={Rectangle16}
+          className="div3 w-full lg:w-[270px] h-[200px] sm:h-[255px] object-cover"
+        />
+        <img
+          src={Rectangle17}
+          className="div4 w-full lg:w-[470px] h-[200px] sm:h-[255px] object-cover"
+        />
+        <img
+          src={Rectangle18}
+          className="div5 w-full lg:w-[370px] h-[200px] sm:h-[255px] object-cover"
+        />
       </div>
-      <MyBtn classname="flex justify-center items-center px-5 py-3 bg-[#2C2C2C] text-white w-[221.81px] self-end">
+
+      <MyBtn classname="flex justify-center items-center px-5 py-3 bg-[#2C2C2C] text-white w-fit self-center lg:self-end mt-10">
         ALL PROJECTS <ArrowRIght classname="inline text-2xl" />
       </MyBtn>
     </section>
